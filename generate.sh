bed855133e751068e12383dd1dafd116b47023ca
Daniel <71837281+darmiel@users.noreply.github.com>
2022-05-16 13:35:10 +0200

    ci: readme updater

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# header
+echo "# cards"
+echo "List of cards in this repo"
+echo ""
+
+# table header
+echo "| Num | Front |"
+
+i=0
+
+# find all .card files
+for c in $(find . -name '*.card'); do
+  FRONT=$(cat "$c" | sed '/--- {{#front/,/--- {{#back/!d' | sed '1d' | sed '$d')
+
+  i=$((i+1))
+  echo "| $i | $FRONT |"
+done
